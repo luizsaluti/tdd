13d5279e5a58b56b1fff8595b5b0ac6f97177eb7
12260032737 <12260032737@Theokoles.casnav1.mb>
2018-08-15 16:24:45 -0300

    Mais testes e relatório

--- Exercicio1/src/CamelCaseConverter.java
+++ Exercicio1/src/CamelCaseConverter.java
@@ -1,11 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CamelCaseConverter {
 
-	public static void converterCamelCase(String fraseCamel) {
-		if(fraseCamel != null) {
-			if(fraseCamel.substring(0, 1).matches("[0-9]")){
-				throw new InicioNumericoException("String não pode inicar com número");
-			}
+	public static List<String> converterCamelCase(String fraseCamel) {
+		List<String> palavras = null;
+		if (fraseCamel != null && fraseCamel.substring(0, 1).matches("[0-9]")) {
+			throw new InicioNumericoException("String não pode iniciar com número");
 		}
+
+	    String fraseMarcada = marcarFrase(fraseCamel);
+		palavras = Arrays.asList(fraseMarcada.split("\\|"));
+	    
+		return palavras.stream().map(palavra -> palavra.toLowerCase()).collect(Collectors.toList());//convertendo todas as palavras para caixa baixa
+	}
+	
+	protected static String marcarFrase(String frase) {
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < frase.length() ; i++) {
+			boolean letraAtualMaiuscula = Character.isUpperCase(frase.charAt(i)); 
+			boolean proximaLetraMinuscula = frase.length() > i + 1 && Character.isLowerCase(frase.charAt(i + 1));
+			boolean letraAnteriorMinuscula = i > 0 && Character.isLowerCase(frase.charAt(i - 1));
+			if (letraAtualMaiuscula && (proximaLetraMinuscula || letraAnteriorMinuscula)) {
+				sb.append("|");
+			}
+			sb.append(frase.charAt(i));
+		}
+		return sb.toString();
 	}
 }
--- Exercicio1/src/CamelCaseTests.java
+++ Exercicio1/src/CamelCaseTests.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Categories.ExcludeCategory;
 
@@ -13,5 +17,33 @@ public class CamelCaseTests {
   public void aceitaInicioNaoNumerico() {
 	  String palavraIniciadaPorLetra = "abc";
 	  CamelCaseConverter.converterCamelCase(palavraIniciadaPorLetra);
+  }
+  
+  @Test
+  public void marcarPalavra() {
+	  String frase = "testeCamelCase";
+	  assertEquals("teste|Camel|Case",CamelCaseConverter.marcarFrase(frase));
+  }
+
+  @Test
+  public void marcarPalavraComSigla() {
+	  String fraseComSigla = "numeroCPFContribuinte";
+	  assertEquals("numero|CPF|Contribuinte",CamelCaseConverter.marcarFrase(fraseComSigla));
+  }
+  
+  @Test
+  public void divideEmArray() {
+	  String frase = "testeCamelCase";
+	  List<String> frases = CamelCaseConverter.converterCamelCase(frase);
+	  assertTrue(frases.size() == 3);
+  }
+  
+  @Test
+  public void conferePalavrasNormais() {
+	  String frase = "testeCamelCase";
+	  List<String> frases = CamelCaseConverter.converterCamelCase(frase);
+	  assertEquals("teste", frases.get(0));
+	  assertEquals("camel", frases.get(1));
+	  assertEquals("case", frases.get(2));
   }
 }
